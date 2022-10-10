38a8b9c13360eb26a5c511f85692002c17c81d42
Experiment2601 <aniketgavas18@gmail.com>
2022-10-10 17:06:39 +0530

    Aniket Day1 Assignment

---
+++ CommandLine.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.day1problems;
+
+public class CommandLine {
+		public static void main(String args[]) {
+			String name = args[0];
+			System.out.println("Your Namne:" +name);
+
+	}
+
+}
